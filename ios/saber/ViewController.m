b53faeb07a7339b0b9b43f3f4610b4ecac998a80
hanjianqiao <hanjianqiao@gmail.com>
2017-10-20 19:05:23 +0800

    update: can use local jsbundle file

--- ios/saber/ViewController.m
+++ ios/saber/ViewController.m
@@ -11,55 +11,65 @@
 #import <React/RCTRootView.h>
 
 @interface ViewController ()
-
-@end
+  
+  @end
 
 @implementation ViewController
-
+  
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
 }
-
+  
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 - (IBAction)but:(id)sender {
-    NSLog(@"High Score Button Pressed");
-    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
-    
-    RCTRootView *rootView =
-    [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
-                                moduleName: @"saber"
-                         initialProperties:
-     @{
-       @"scores" : @[
-               @{
-                   @"name" : @"Alex",
-                   @"value": @"42"
-                   },
-               @{
-                   @"name" : @"Joel",
-                   @"value": @"10"
-                   }
-               ]
-       }
-                             launchOptions: nil];
-    UIViewController *vc = [[UIViewController alloc] init];
-    vc.view = rootView;
-    //[self presentViewController:vc animated:YES completion:nil];
-    [self.navigationController pushViewController:vc animated:YES];
-}
-    
-/*
-#pragma mark - Navigation
+  NSLog(@"High Score Button Pressed");
+  NSURL *jsCodeLocation;
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+# if 0
+  // get bundle file from server
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+# else
+  // if choose this bundle file run command at project root:
+  // react-native bundle --platform ios --dev false --entry-file index.js --bundle-output ios/main.jsbundle
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+# endif
+  
+  RCTRootView *rootView =
+  [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+                              moduleName: @"saber"
+                       initialProperties:
+   @{
+     @"scores" : @[
+         @{
+           @"name" : @"Alex",
+           @"value": @"42"
+           },
+         @{
+           @"name" : @"Joel",
+           @"value": @"10"
+           }
+         ]
+     }
+                           launchOptions: nil];
+  UIViewController *vc = [[UIViewController alloc] init];
+  vc.view = rootView;
+  //[self presentViewController:vc animated:YES completion:nil];
+  [self.navigationController pushViewController:vc animated:YES];
 }
-*/
+  
+  /*
+   #pragma mark - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+   // Get the new view controller using [segue destinationViewController].
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+  @end
 
-@end
